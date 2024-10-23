984532149bd53826ab64a35a6eb98d1de24faec3
Ted Poole <tpoole@redhat.com>
2024-10-23 15:10:34 +0100

    Reorganised so tmt_path is default again (.)

--- ci/run_envoy_docker.sh
+++ ci/run_envoy_docker.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-docker run --rm -t fedora sh -c "$*"
+echo "HELLO FROM  $0 $*"
+echo
+echo "pwd = $(pwd)"
+echo
+echo "============================== set ======================================="
+set
+echo "============================== set ======================================="
+echo
+echo "============================== dir ======================================="
+find . -ls | grep -F -v ' ./.git/'
+echo "============================== dir ======================================="
+echo
+echo "Launching do_ci.sh in docker now..."
+echo
+docker run --rm -t -v "$(dirname "$0")/..:/work" -w /work fedora sh -c "$*"
